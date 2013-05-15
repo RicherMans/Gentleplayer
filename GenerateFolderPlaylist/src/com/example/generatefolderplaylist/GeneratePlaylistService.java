0b9fed88f7c154038c4acaf57db0c970c89ad316
richman <richman@richman-laz0r>
2013-05-15 21:33:59 +0200

    Added contentresolver update for playlists

--- GenerateFolderPlaylist/src/com/example/generatefolderplaylist/GeneratePlaylistService.java
+++ GenerateFolderPlaylist/src/com/example/generatefolderplaylist/GeneratePlaylistService.java
@@ -1,34 +1,25 @@
 package com.example.generatefolderplaylist;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 import android.app.Service;
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.content.ContentValues;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.media.MediaScannerConnection;
-import android.media.MediaScannerConnection.MediaScannerConnectionClient;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.IBinder;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Audio.Playlists;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
-import android.widget.Toast;
 
 public class GeneratePlaylistService extends Service {
 
@@ -49,41 +40,37 @@ public class GeneratePlaylistService extends Service {
 		super.onCreate();
 	}
 
-	private void writeFile(String folderPath, ArrayDeque<String> list) {
-		String tmp[] = folderPath.split("/");
-		if (tmp != null) {
-			String playlistName = tmp[tmp.length - 1];
-			File playlistPa = new File(PLAYLISTPATH);
-			if (!playlistPa.exists()) {
-				playlistPa.mkdir();
+	private void writeFile(String playlistName, ArrayDeque<String> list) {
+		File playlistPa = new File(PLAYLISTPATH);
+		if (!playlistPa.exists()) {
+			playlistPa.mkdir();
+		}
+		File f = new File(PLAYLISTPATH + playlistName + ".m3u");
+		BufferedWriter out = null;
+		try {
+			if (f.exists()) {
+				f.delete();
 			}
-			File f = new File(PLAYLISTPATH + playlistName + ".m3u");
-			BufferedWriter out = null;
-			try {
-				if (f.exists()) {
-					f.delete();
-				}
-				f.createNewFile();
-				out = new BufferedWriter(new FileWriter(f));
-				StringBuffer m3UDat = new StringBuffer();
-				for (String song : list) {
-					m3UDat.append(song);
-					m3UDat.append("\n");
-				}
-				out.write(m3UDat.toString());
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				// java 7 pro
-				if (out != null)
-					try {
-						out.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+			f.createNewFile();
+			out = new BufferedWriter(new FileWriter(f));
+			StringBuffer m3UDat = new StringBuffer();
+			for (String song : list) {
+				m3UDat.append(song);
+				m3UDat.append("\n");
 			}
+			out.write(m3UDat.toString());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// java 7 pro
+			if (out != null)
+				try {
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 		}
 	}
 
@@ -91,18 +78,27 @@ public class GeneratePlaylistService extends Service {
 		this.stopSelf();
 	}
 
-	private class SearchTreeJob extends
-			AsyncTask<String, ArrayDeque<String>, HashMap<String, ArrayDeque<String>>>  {
+	private class SearchTreeJob
+			extends
+			AsyncTask<String, ArrayDeque<String>, HashMap<String, ArrayDeque<String>>> {
 		@Override
 		protected void onPostExecute(HashMap<String, ArrayDeque<String>> result) {
 			super.onPostExecute(result);
 			if (result != null) {
 				for (String key : result.keySet()) {
+//					ContentValues values = new ContentValues();
 					Log.d("Key V", key + result.get(key));
-					writeFile(key, result.get(key));
+					String tmpA[] = key.split("/");
+					String playlistName = tmpA[tmpA.length - 1];
+					Log.d("Media V",playlistName);
+//					values.put(Playlists.NAME, playlistName);
+					writeFile(playlistName, result.get(key));
+//					getContentResolver().update(
+//							MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI,
+//							values,null,null);
 				}
-				//TODO: Use contentresolver to manually add the Playlist files into Androids DB
-				sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://"+PLAYLISTPATH)));
+				// TODO: Use contentresolver to manually add the Playlist files
+				// into Androids DB
 				stop();
 			}
 		}
@@ -114,54 +110,23 @@ public class GeneratePlaylistService extends Service {
 		}
 
 		@Override
-		protected HashMap<String, ArrayDeque<String>> doInBackground(String... params) {
+		protected HashMap<String, ArrayDeque<String>> doInBackground(
+				String... params) {
 			if (params.length > 1) {
 				return null;
 			}
-			
+
 			File root = new File(params[0]);
 			if (root != null) {
 				HashMap<String, ArrayDeque<String>> pathstomp = new HashMap<String, ArrayDeque<String>>();
 				try {
-					File playlistPa = new File(PLAYLISTPATH);
-					// Playlist file were already created
-					if (playlistPa.exists()) {
-						for (File m3u : playlistPa.listFiles()) {
-							// We only need track 1 because that will give us
-							// the parent path to all other tracks
-							pathstomp = searchNodesRec(new File(readFirstTrackInM3u(m3u)),pathstomp);
-						}
-					} else {
-						pathstomp = searchNodesRec(root, pathstomp);
-					}
-
+					pathstomp = searchNodesRec(root, pathstomp);
 					return pathstomp;
 				} catch (MalformedURLException e) {
 					stop();
 				}
 			}
 			return null;
-		}
-
-		private String readFirstTrackInM3u(File m3u) {
-			BufferedReader reader=null;
-			String conLine = new String();
-			try {
-				reader = new BufferedReader(
-						new InputStreamReader(new FileInputStream(m3u)));
-				String line = reader.readLine();
-				conLine = line.substring(0, line.lastIndexOf("/")+1);
-			} catch (FileNotFoundException e) {
-				System.err.println(e);
-			} catch (IOException e) {
-				System.err.println(e);
-			}
-			try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return conLine;
 		}
 
 		private HashMap<String, ArrayDeque<String>> searchNodesRec(File dir,
--- GenerateFolderPlaylist/src/com/example/generatefolderplaylist/MainStartActivity.java
+++ GenerateFolderPlaylist/src/com/example/generatefolderplaylist/MainStartActivity.java
@@ -1,19 +1,26 @@
 package com.example.generatefolderplaylist;
+
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Audio.Media;
+import android.util.Log;
 
 //This acitivity will not be displayed, instead it is onley used to start the service
 public class MainStartActivity extends Activity {
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		finish();
 	}
+
 }
